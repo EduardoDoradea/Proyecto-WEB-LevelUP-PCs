fe929647a97562261dbacef4dd91f4e64bd8331c
Samuelsf05 <00026724@uca.edu.sv>
2025-11-23 12:35:57 -0600

    refactor: Home Page
    refactorización de la Home page

--- Frontend/LevelUp-frontend/src/App.jsx
+++ Frontend/LevelUp-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Home/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CatalogPage from "./pages/CatalogPage";
