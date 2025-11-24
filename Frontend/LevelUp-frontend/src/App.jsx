f93c656a76ea3930806e6da45bedff9d6ebe99c8
Samuelsf05 <00026724@uca.edu.sv>
2025-11-24 15:52:46 -0600

    feat: Agregar Asesoría técnica
    Agregando css para la pagina de asesorías

--- Frontend/LevelUp-frontend/src/App.jsx
+++ Frontend/LevelUp-frontend/src/App.jsx
@@ -16,6 +16,7 @@ import WattCalculatorPage from "./pages/WattCalculator/WattCalculatorPage";
 import WarrantyPage from "./pages/Warranty/WarrantyPage";
 import MaintenancePage from "./pages/Maintenance/MaintenancePage";
 import FAQPage from "./pages/FAQ/FAQPage";
+import TechAdvisoryPage from "./pages/TechAdvisory/TechAdvisoryPage";
 import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
 export default function App() {
@@ -43,6 +44,7 @@ export default function App() {
           <Route path="/soporte/garantías" element={<WarrantyPage />} />
           <Route path="/soporte/mantenimiento" element={<MaintenancePage />} />
           <Route path="/soporte/faq" element={<FAQPage />} />
+          <Route path="/soporte/asesoría-técnica" element={<TechAdvisoryPage />} />
           
           <Route path="/nosotros/quiénes-somos" element={<AboutPage />} />
           <Route path="/nosotros/ubicación" element={<LocationPage />} />
