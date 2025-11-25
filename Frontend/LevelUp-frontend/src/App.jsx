1fac439223bf0bed3c7bb281b2f8409a73c3c058
Samuelsf05 <00026724@uca.edu.sv>
2025-11-25 01:20:12 -0600

    feat: Acceder a productos desde la BD

--- Frontend/LevelUp-frontend/src/App.jsx
+++ Frontend/LevelUp-frontend/src/App.jsx
@@ -19,6 +19,9 @@ import FAQPage from "./pages/FAQ/FAQPage";
 import TechAdvisoryPage from "./pages/TechAdvisory/TechAdvisoryPage";
 import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
+// 🧪 COMPONENTE DE PRUEBA TEMPORAL - ELIMINAR DESPUÉS DE VERIFICAR API
+import TestAPI from "./pages//TestApi";
+
 export default function App() {
   return (
     <CartProvider>
@@ -27,6 +30,9 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/registro" element={<RegisterPage />} />
+
+           {/* 🧪 RUTA DE PRUEBA TEMPORAL */}
+          <Route path="/test-api" element={<TestAPI />} />
           
           <Route path="/componentes" element={<CategoriesPage />} />
           
